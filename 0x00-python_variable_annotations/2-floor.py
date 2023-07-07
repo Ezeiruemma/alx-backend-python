78e6e0de58fa14b5191e0ab9a3ec736406b45715
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-07-07 03:21:42 +0100

    task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
-'''
-type-annotated function
-'''
+
+"""
+python3 -c
+'print(__import__("2-floor").__doc__)'
+Write a type-annotated function floor which takes a float n as argument and returns the floor of the float.
+
+Author: Oluwaseun Ayela 
+"""
+
+import math
 
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+def floor(n: float) -> int:
+    """
+    Trying this docstring for this module
+    python3 -c
+    'print(__import__("2-floor").floor.__doc__)'
+
+    """
+    return math.floor(n)
+    python3 - c
+    print(__import__("2-floor").floor.__doc__)
