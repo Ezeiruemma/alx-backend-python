faaf79ba53e48f3d7e32f64f7b110d82b0fb3906
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-07-07 04:21:00 +0100

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
-"""
-python3 -c
-'print(__import__("9-element_length").__doc__)'
-Annotate the below function’s parameters and return values with the appropriate types
+'''
+type-annotated function
+'''
+from typing import Iterable, List, Sequence, Tuple
 
 
-Author: Oluwaseun Ayela 
-"""
-
-from typing import List, Tuple
-
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """
-    Trying this docstring for this module
-     python3 -c
-     'print(__import__("9-element_length").element_length.__doc__)'
-
-    """
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Computes  and return the length of a list of sequences.
+    '''
     return [(i, len(i)) for i in lst]
-    python3 - c
-    print(__import__("9-element_length").element_length.__doc__)'
