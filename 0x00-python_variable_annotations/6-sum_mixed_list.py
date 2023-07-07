a784d0416afae1fb9f9a5768addb7c6512ed1644
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-07-07 04:19:50 +0100

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
-"""
-python3 -c
-'print(__import__("5-sum_list").__doc__)'
-Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
-
-Author: Oluwaseun Ayela 
-"""
-
-from typing import List, Union
+'''
+type-annotated function
+'''
+from typing import List
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """
-    Trying this docstring for this module
-     python3 -c
-     'print(__import__("5-sum_list").sum_mixed_list.__doc__)'
-
-    """
-    return sum(mxd_lst)
-    python3 - c
-    print(__import__("5-sum_list").sum_mixed_list.__doc__)
+def sum_list(input_list: List[float]) -> float:
+    '''takes list of float and reture summed value 
+    '''
+    return float(sum(input_list))
